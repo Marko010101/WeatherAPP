53a1424420594785fa501e733845ebc18ae9b1ce
Marko <baghashvili.marko@gmail.com>
2024-01-21 22:33:11 +0400

    Finished responsiveness at 376px

---
+++ src/javascript/scroll.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const header = document.querySelector("header");
+
+function handleScroll() {
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 1) {
+    header.classList.add("header-scrolled");
+  } else {
+    header.classList.remove("header-scrolled");
+  }
+}
+
+window.addEventListener("scroll", handleScroll);
