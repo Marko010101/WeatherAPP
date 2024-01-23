f90527177feefaccacde8da8cd1dd4d94acdfd60
Marko <baghashvili.marko@gmail.com>
2024-01-24 00:17:29 +0400

    Update scroll.js

--- src/javascript/scroll.js
+++ src/javascript/scroll.js
@@ -1,19 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   const header = document.querySelector("header");
 
-  if (header) {
-    function handleScroll() {
-      const scrollPosition = window.scrollY;
+  function handleScroll() {
+    const scrollPosition = window.scrollY;
 
-      if (scrollPosition > 1) {
-        header.classList.add("header-scrolled");
-      } else {
-        header.classList.remove("header-scrolled");
-      }
+    if (scrollPosition > 1) {
+      header.classList.add("header-scrolled");
+    } else {
+      header.classList.remove("header-scrolled");
     }
-
-    window.addEventListener("scroll", handleScroll);
-  } else {
-    console.error("Header element not found");
   }
+
+  window.addEventListener("scroll", handleScroll);
 });
