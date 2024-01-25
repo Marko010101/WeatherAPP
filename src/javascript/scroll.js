c1538ed3d0fe439cb0c9cdfb6783512256e5298f
Marko <baghashvili.marko@gmail.com>
2024-01-26 00:17:53 +0400

    Finishing 375 and 420 responsive designs

--- src/javascript/scroll.js
+++ src/javascript/scroll.js
@@ -1,6 +1,6 @@
+const header = document.querySelector("header");
+const mobileHeader = document.querySelector(".header-mobile");
 document.addEventListener("DOMContentLoaded", function () {
-  const header = document.querySelector("header");
-
   function handleScroll() {
     const scrollPosition = window.scrollY;
 
@@ -13,3 +13,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
   window.addEventListener("scroll", handleScroll);
 });
+
+//  If the mobile-header is active, the header will disappear on scrolling down.
+if (mobileHeader) {
+  let prevScrollPos = window.pageYOffset;
+
+  window.addEventListener("scroll", function () {
+    const currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos < currentScrollPos) {
+      // Scrolling down
+      header.style.transform = "translateY(-100%)";
+    } else {
+      // Scrolling up
+      header.style.transform = "translateY(0)";
+    }
+
+    prevScrollPos = currentScrollPos;
+  });
+}
