ae1fdd29e1d841fbea20dfa76444843a7ec5edc2
Marko <baghashvili.marko@gmail.com>
2024-01-27 22:38:28 +0400

    Update sidebar.js

--- src/javascript/sidebar.js
+++ src/javascript/sidebar.js
@@ -4,30 +4,41 @@ document.addEventListener("DOMContentLoaded", function () {
   const mobileMenu = document.getElementById("mobileMenu");
   const closeMenu = document.getElementById("closeMenu");
 
+  function toggleMenu(event) {
+    event.stopPropagation();
+    mobileMenu.style.right = "0";
+    body.classList.add("no-scroll", "sidebarFilter");
+    mobileMenu.style.filter = "brightness(1.65)";
+    mobileMenu.style.top =
+      (document.documentElement.scrollTop || document.body.scrollTop) + "px";
+  }
+
+  function closeMobileMenu() {
+    mobileMenu.style.filter = "brightness(1)";
+    mobileMenu.style.right = "-100%";
+    body.classList.remove("no-scroll", "sidebarFilter");
+  }
+
   if (window.innerWidth < 750) {
-    menuIcon.addEventListener("click", function (event) {
+    // Click and touchstart for menu icon
+    menuIcon.addEventListener("click", toggleMenu);
+    menuIcon.addEventListener("touchstart", toggleMenu);
+
+    // Click and touchstart for close icon
+    closeMenu.addEventListener("click", closeMobileMenu);
+    closeMenu.addEventListener("touchstart", closeMobileMenu);
+
+    // Click and touchstart for document
+    document.addEventListener("click", closeMobileMenu);
+    document.addEventListener("touchstart", closeMobileMenu);
+
+    // If the user clicks inside the menu or on the menu icon, prevent closing
+    mobileMenu.addEventListener("click", function (event) {
       event.stopPropagation();
-      mobileMenu.style.right = "0";
-      body.classList.add("no-scroll", "sidebarFilter");
-      mobileMenu.style.filter = "brightness(1.65)";
-      mobileMenu.style.top = document.documentElement.scrollTop + "px";
     });
 
-    closeMenu.addEventListener("click", function () {
-      mobileMenu.style.filter = "brightness(1)";
-      mobileMenu.style.right = "-100%";
-      body.classList.remove("no-scroll", "sidebarFilter");
-    });
-
-    // If the user clicks anything else instead of the menu, the menu will be closed
-    document.addEventListener("click", function (event) {
-      const isClickInsideMenu = mobileMenu.contains(event.target);
-      const isClickOnMenuIcon = menuIcon.contains(event.target);
-
-      if (!isClickInsideMenu && !isClickOnMenuIcon) {
-        mobileMenu.style.right = "-100%";
-        body.classList.remove("no-scroll", "sidebarFilter");
-      }
+    menuIcon.addEventListener("touchstart", function (event) {
+      event.stopPropagation();
     });
   }
 });
