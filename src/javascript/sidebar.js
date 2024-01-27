d1b3230234a8861425c5c95c1d30ef73482ca739
Marko <baghashvili.marko@gmail.com>
2024-01-28 00:00:07 +0400

    Update sidebar.js

--- src/javascript/sidebar.js
+++ src/javascript/sidebar.js
@@ -1,42 +1,33 @@
-const body = document.querySelector("body");
-const menuIcon = document.getElementById("menuIcon");
-const mobileMenu = document.getElementById("mobileMenu");
-const closeMenu = document.getElementById("closeMenu");
+document.addEventListener("DOMContentLoaded", function () {
+  const body = document.querySelector("body");
+  const menuIcon = document.getElementById("menuIcon");
+  const mobileMenu = document.getElementById("mobileMenu");
+  const closeMenu = document.getElementById("closeMenu");
 
-function toggleMenu(event) {
-  event.stopPropagation();
-  mobileMenu.style.right = "0";
-  body.classList.add("no-scroll", "sidebarFilter");
-  mobileMenu.style.filter = "brightness(1.65)";
-  mobileMenu.style.top =
-    (document.documentElement.scrollTop || document.body.scrollTop) + "px";
-}
+  if (window.innerWidth < 750) {
+    menuIcon.addEventListener("click", function (event) {
+      event.stopPropagation();
+      mobileMenu.style.right = "0";
+      body.classList.add("no-scroll", "sidebarFilter");
+      mobileMenu.style.filter = "brightness(1.65)";
+      mobileMenu.style.top = document.documentElement.scrollTop + "px";
+    });
 
-function closeMobileMenu() {
-  mobileMenu.style.filter = "brightness(1)";
-  mobileMenu.style.right = "-100%";
-  body.classList.remove("no-scroll", "sidebarFilter");
-}
+    closeMenu.addEventListener("click", function () {
+      mobileMenu.style.filter = "brightness(1)";
+      mobileMenu.style.right = "-100%";
+      body.classList.remove("no-scroll", "sidebarFilter");
+    });
 
-if (window.innerWidth < 750) {
-  // Click and touchstart for menu icon
-  menuIcon.addEventListener("click", toggleMenu);
-  menuIcon.addEventListener("touchstart", toggleMenu);
+    // If the user clicks anything else instead of the menu, the menu will be closed
+    document.addEventListener("click", function (event) {
+      const isClickInsideMenu = mobileMenu.contains(event.target);
+      const isClickOnMenuIcon = menuIcon.contains(event.target);
 
-  // Click and touchstart for close icon
-  closeMenu.addEventListener("click", closeMobileMenu);
-  closeMenu.addEventListener("touchstart", closeMobileMenu);
-
-  // Click and touchstart for document
-  document.addEventListener("click", closeMobileMenu);
-  document.addEventListener("touchstart", closeMobileMenu);
-
-  // If the user clicks inside the menu or on the menu icon, prevent closing
-  mobileMenu.addEventListener("click", function (event) {
-    event.stopPropagation();
-  });
-
-  menuIcon.addEventListener("touchstart", function (event) {
-    event.stopPropagation();
-  });
-}
+      if (!isClickInsideMenu && !isClickOnMenuIcon) {
+        mobileMenu.style.right = "-100%";
+        body.classList.remove("no-scroll", "sidebarFilter");
+      }
+    });
+  }
+});
